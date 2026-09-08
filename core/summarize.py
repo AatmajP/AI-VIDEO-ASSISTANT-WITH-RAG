3884682b4cc4dd5d57870d65434fd693bdf01896
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-09 00:05:27 +0530

    Enhance summarize function to combine partial summaries into a final professional meeting summary

--- core/summarize.py
+++ core/summarize.py
@@ -34,3 +34,20 @@ def summarize(transcript : str) -> str:
     chunk_summaries = [map_chain.invoke({"text" : chunk}) for chunk in chunks]
 
     combined = "\n\n".join(chunk_summaries)
+
+    combined_prompt = ChatPromptTemplate.from_messages(
+        [
+        (
+            "system",
+            "You are an expert meeting summarizer. Combine these partial summaries "
+            "into one final professional meeting summary in bullet points.",
+        ),
+        ("human", "{text}"),
+    ]
+    )
+
+    combined_chain = (
+        RunnablePassthrough() | RunnableLambda(lambda x:{"text":x}) | combined_prompt | llm | StrOutputParser()
+    )
+
+    return combined_chain.invoke(combined)
