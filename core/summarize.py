5ba7d5f92320ba87c122d8a941cdaada57703d3c
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-08 23:58:22 +0530

    Add newline at end of file and format get_llm function

--- core/summarize.py
+++ core/summarize.py
@@ -5,3 +5,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
 import os 
+
+def get_llm():
+    return ChatGroq(model="openai/gpt-oss-20b", groq_api_key=os.getenv("GROQ_API_KEY"),temperature=0.3)
