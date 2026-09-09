bc1a24c311a933c6375200d53e077808f4aa901d
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-09 19:14:28 +0530

    Add functions to initialize LLM and build processing chain for transcripts

--- core/extractor.py
+++ core/extractor.py
@@ -3,3 +3,15 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 import os 
+
+def get_llm():
+    return ChatGroq(model="openai/gpt-oss-20b", groq_api_key=os.getenv("GROQ_API_KEY"),temperature=0.3)
+
+def build_chain(system_prompt : str):
+    llm = get_llm()
+    return (
+        RunnablePassthrough() | RunnableLambda(lambda x : {"text" : x}) |ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human","{text}"),
+    ]) | llm |StrOutputParser()
+    )
