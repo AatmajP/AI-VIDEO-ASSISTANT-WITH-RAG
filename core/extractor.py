e2f0430b93a1a6bd638750e6dd7977235ce18341
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-09 19:09:55 +0530

    Add initial imports for extractor module

---
+++ core/extractor.py
@@ -0,0 +1,5 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+import os 
