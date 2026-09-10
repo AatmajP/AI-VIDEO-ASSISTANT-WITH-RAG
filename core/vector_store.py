b42cf498224f8901a31126bc54a31a17f819e760
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-10 23:31:00 +0530

    Fix formatting and ensure consistent newline at the end of vector_store.py

--- core/vector_store.py
+++ core/vector_store.py
@@ -3,3 +3,7 @@ from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
+
+CHROMA_DIR = "vector_db"
+COLLECTION_NAME = "meeting_transcript"
+EMBEDDING_MODEL  = "all-MiniLM-L6-v2"
