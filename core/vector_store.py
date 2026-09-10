fbc9eae14394214b46b85fdfabe54175af40f81a
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-10 23:33:27 +0530

    Add load_vector_store and get_retriever functions to vector store module

--- core/vector_store.py
+++ core/vector_store.py
@@ -37,3 +37,19 @@ def build_vector_store(transcript : str)->Chroma:
     )
 
     return vector_store
+
+def load_vector_store() ->Chroma:
+    embeddings = get_embeddings()
+    vector_store = Chroma(
+        collection_name=COLLECTION_NAME,
+        embedding_function= embeddings,
+        persist_directory=CHROMA_DIR
+    )
+
+    return vector_store
+
+def get_retriever(vector_store : Chroma, k :int = 4):
+    return vector_store.as_retriever(
+        search_type = 'similarity',
+        search_kwargs = {"k":k}
+    )
