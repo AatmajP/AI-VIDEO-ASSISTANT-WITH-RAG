372a7fd655716a759cc6e6d285add5fcb5995c50
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-14 21:45:08 +0530

    Fix typo in comment for time import in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import time
+import time # used fgor simulating progress updates
 from dotenv import load_dotenv
 from utils.audio_processor import process_input
 from core.transcriber import transcribe_all
