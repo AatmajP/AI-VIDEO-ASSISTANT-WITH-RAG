6c3c30a6301c891329ab6f26f70e76bc5bc6c5b8
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-16 22:24:17 +0530

    Clarify comment on environment variable usage in extractor.py

--- core/extractor.py
+++ core/extractor.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
-import os # used to get environment variables
+import os # used to get environment variables like GROQ_API_KEY
 
 def get_llm():
     return ChatGroq(model="openai/gpt-oss-20b", groq_api_key=os.getenv("GROQ_API_KEY"),temperature=0.3)
