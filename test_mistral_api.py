adf4b0f348201d32e87441871a1f33e16b327cfd
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-17 22:21:30 +0530

    Fix comment formatting for load_dotenv in test_mistral_api.py

--- test_mistral_api.py
+++ test_mistral_api.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # MUST be before any core/ imports
 from langchain_groq import ChatGroq
 model = ChatGroq(model="openai/gpt-oss-20b")
 response = model.invoke("full form of irctc") #we can also use the model.invoke_async() method to get the response asynchronously
