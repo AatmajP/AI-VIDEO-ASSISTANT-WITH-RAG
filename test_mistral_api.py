1d53bb2abbf392c0572d0f5bfa6b17b3a75728c0
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-17 22:20:38 +0530

    Add comment to clarify asynchronous response option in test_mistral_api.py

--- test_mistral_api.py
+++ test_mistral_api.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
 model = ChatGroq(model="openai/gpt-oss-20b")
-response = model.invoke("full form of irctc")
+response = model.invoke("full form of irctc") #we can also use the model.invoke_async() method to get the response asynchronously
 print(response.content)
