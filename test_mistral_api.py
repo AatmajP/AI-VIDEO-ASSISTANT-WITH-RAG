593e0c1dafc2d24c056c1e747f52e2d7f7bbadcd
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-18 19:55:53 +0530

    Fix model instantiation to use the correct version of ChatGroq

--- test_mistral_api.py
+++ test_mistral_api.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # MUST be before any core/ imports
 from langchain_groq import ChatGroq # used groq
-model = ChatGroq(model="openai/gpt-oss-20b")
+model = ChatGroq(model="openai/gpt-oss-0b")
 response = model.invoke("full form of irctc") #we can also use the model.invoke_async() method to get the response asynchronously
 print(response.content)
