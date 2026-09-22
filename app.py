7f3269737ce8ff707ebb49d5ec3bbe7c63911454
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-22 21:51:24 +0530

    Fix formatting issue by adding a trailing space in chat_history initialization in app.py

--- app.py
+++ app.py
@@ -367,9 +367,9 @@ if run_btn:
     else:
         st.session_state.pipeline_done = False
         st.session_state.result = None
-        st.session_state.chat_history = []
+        st.session_state.chat_history = []   
         st.session_state.pipeline_steps = {} 
-        
+
 
         progress_placeholder = st.empty()
 
