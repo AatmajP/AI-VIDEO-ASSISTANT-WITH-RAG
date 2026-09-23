f566f0c4ec87bba83f4a2a5a3bb7587df1cfea29
Aatmaj Patro <aatmajpatro@gmail.com>
2026-09-23 18:23:57 +0530

    Fix formatting issue by adding a trailing space in the import statement for audio_processor in test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()   # MUST be before any core/ imports
 
-from utils.audio_processor import process_input 
+from utils.audio_processor import process_input  
 from core.transcriber import transcribe_all
 from core.summarize import summarize, generate_title
 from core.extractor import extract_action_items, extract_key_decisions, extract_questions
